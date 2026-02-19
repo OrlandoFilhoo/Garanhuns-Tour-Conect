8de21ab6c642d86566e73c518c8ba3fd3cb1b933
OrlandoFilhoo <nandofilho2023@gmail.com>
2026-02-19 18:09:02 -0300

    Animation

--- static/script.js
+++ static/script.js
@@ -7,3 +7,25 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+//animation
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+  animeScroll();
+});
